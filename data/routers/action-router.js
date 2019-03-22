7dfabbaa6e7de7d575467652c92fc67343cf7739
ddot727 <ddot727@gmail.com>
2019-03-22 13:38:06 -0400

    created the rest of the CRUD endpoints for actions

--- data/routers/action-router.js
+++ data/routers/action-router.js
@@ -14,4 +14,54 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  actionDb.get(id)
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "error getting action"
+      })
+    })
+})
+
+router.post("/", async (req, res) => {
+  try {
+    const action = await actionDb.insert(req.body)
+    res.status(201).json(action)
+  } catch (error) {
+    res.status(500).json({
+      message: "error adding action"
+    })
+  }
+})
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const update = await actionDb.update(id, req.body);
+    res.status(200).json(update);
+  } catch (error) {
+    res.status(500).json({
+      message: "error updating action"
+    })
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const remove = await actionDb.remove(id);
+    res.status(200).json({
+      message: "Delete Successful"
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "error deleting action"
+    })
+  }
+})
+
 module.exports = router;
