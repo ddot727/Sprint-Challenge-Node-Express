e9059b285a4168a84398d56ef33fa409153ee2f3
ddot727 <ddot727@gmail.com>
2019-03-22 14:09:03 -0400

    created the rest of the CRUD endpoints for projects, projects by id, and project actions

--- data/routers/project-router.js
+++ data/routers/project-router.js
@@ -20,7 +20,9 @@ router.get("/:id", async (req, res) => {
     const project = await projectDb.get(id);
     res.status(200).json(project);
   } catch (error) {
-    res.status(500).json({ message: "error retrieving project" });
+    res.status(500).json({ 
+      message: "error retrieving project" 
+    });
   }
 });
 
@@ -31,6 +33,41 @@ router.get("/:id/actions", (req,res) => {
       res.status(500).json({
         message: "error getting project actions"
       }))
-})
+});
+
+router.post("/", (req, res) => {
+  projectDb
+    .insert(req.body)
+    .then(post => res.status(200).json(post))
+    .catch(err => res.status(500).json({ 
+      message: "error posting"
+    }));
+});
+
+ router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const change = await projectDb.update(id, req.body);
+    res.status(200).json(change);
+  } catch (error) {
+    res.status(500).json({ 
+      message: "error updating" 
+    });
+  }
+});
+
+ router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await projectDb.remove(id);
+    res.status(200).json({ 
+      message: "successfully deleted"
+    });
+  } catch (error) {
+    res.status(500).json({ 
+      message: "error deleting"
+    });
+  }
+});
 
 module.exports = router;
